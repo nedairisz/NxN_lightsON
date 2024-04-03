788c8a1d633b9447abc30777523dd7d89840668e
iriszneda <iriszneda@gmail.com>
2024-04-03 13:07:56 +0200

    metódusok ki

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -31,15 +31,6 @@ export function esemeny(){
         const targetIndex = mezoElemek.index($(this)); // Az aktuális div indexének meghatározása
         const targetDiv = $(this);
 
-        // Függvény a világító osztály hozzáadására vagy eltávolítására
-        function toggleVilagitClass(element) {
-            element.toggleClass("vilagit");
-        }
-
-        function updateSzamlalo() {
-            const vilagitMezokSzama = mezoElemek.filter('.vilagit').length;
-            szamlaloElem.text(vilagitMezokSzama);
-        }
        
         const szomszedIndexek = [
             [1, 3],     // 0
@@ -60,9 +51,20 @@ export function esemeny(){
 
         // A kattintott mezőre vonatkozó átkapcsolás
         toggleVilagitClass(targetDiv);
-
+        
+        updateSzamlalo()
         lampak()
     });
+}
+
+function updateSzamlalo() {
+    const vilagitMezokSzama = mezoElemek.filter('.vilagit').length;
+    szamlaloElem.text(vilagitMezokSzama);
+}
+
+// Függvény a világító osztály hozzáadására vagy eltávolítására
+function toggleVilagitClass(element) {
+    element.toggleClass("vilagit");
 }
 
 export function lampak(){
